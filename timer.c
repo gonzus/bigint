9166cde0f97dbeaa0ae5b26fe256c5fa451d7de1
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2020-08-27 17:07:28 +0200

    Update timer functions

--- timer.c
+++ timer.c
@@ -1,5 +1,9 @@
+#include <stdio.h>
 #include <time.h>
 #include "timer.h"
+
+#define USECS_IN_A_MSEC  1000UL
+#define USECS_IN_A_SEC   (USECS_IN_A_MSEC * USECS_IN_A_MSEC)
 
 void timer_start(Timer* t) {
     gettimeofday(&t->t0, NULL);
@@ -9,26 +13,24 @@ void timer_stop(Timer* t) {
     gettimeofday(&t->t1, NULL);
 }
 
-unsigned long timer_elapsed(Timer* t)
-{
-    return ((t->t1.tv_sec  - t->t0.tv_sec ) * 1000000 +
+unsigned long timer_elapsed_us(Timer* t) {
+    return ((t->t1.tv_sec  - t->t0.tv_sec ) * USECS_IN_A_SEC +
             (t->t1.tv_usec - t->t0.tv_usec));
 }
 
-void timer_format_elapsed(Timer* t, FILE* fp, int newline)
-{
+void timer_format_elapsed(Timer* t, FILE* fp, int newline) {
     static struct {
         unsigned long min;
         const char* name;
     } units[] = {
-        { 24 * 60 * 60 * 1000000UL, "day" },
-        {      60 * 60 * 1000000UL, "h"   },
-        {           60 * 1000000UL, "min" },
-        {                1000000UL, "s"   },
-        {                   1000UL, "ms"  },
-        {                      0UL, "us"  },  // default
+        { 24UL * 60UL * 60UL * USECS_IN_A_SEC, "D"  },
+        {        60UL * 60UL * USECS_IN_A_SEC, "h"  },
+        {               60UL * USECS_IN_A_SEC, "m"  },
+        {                      USECS_IN_A_SEC, "s"  },
+        {                     USECS_IN_A_MSEC, "ms" },
+        { /* default, catch all */        0UL, "us" },
     };
-    unsigned long us = timer_elapsed(t);
+    unsigned long us = timer_elapsed_us(t);
     for (int j = 0; 1; ++j) {
         if (us > units[j].min) {
             float value = us / (float) units[j].min;
